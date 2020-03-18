655563ad56bf61becad3a1b1814eed9ca144a616
EliteDevSolution <62186848+EliteDevSolution@users.noreply.github.com>
2020-03-19 05:05:27 +1000

    final
    final

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductGetComponent } from './product-get/product-get.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ProductsService } from './products.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductAddComponent,
+    ProductGetComponent,
+    ProductEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [ProductsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
